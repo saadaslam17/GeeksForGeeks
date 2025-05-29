62c56273aed2fc4381bdda64ea72fc444b9200d2
saadaslam17 <mohammadsaad9935@gmail.com>
2025-05-29 16:54:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequency in a Linked List/frequency-in-a-linked-list.cpp
@@ -0,0 +1,33 @@
+/*
+  Node is defined as
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}*head;
+*/
+class Solution {
+  public:
+    int count(struct Node* head, int key) {
+        // add your code here
+        Node *temp = head;
+        int count = 0;
+        
+        while(temp != NULL)
+        {
+            if(temp -> data == key)
+            {
+                count++;
+            }
+            
+            temp = temp -> next;
+        }
+        
+        return count;
+    }
+};
